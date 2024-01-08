11b15e90b542bf8d6092607e6af5e4ec91f35ae9
340854561 <340854561@gapps.yrdsb.ca>
2024-01-08 08:56:18 -0500

    a

--- Main.java
+++ Main.java
@@ -8,17 +8,29 @@ import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.*;
 
-public class Main
+public class Main extends JPanel
 {
     JFrame frame;
+    JPanel panel;
 
     public Main() {
         frame = new JFrame("BomberMan");
         frame.setPreferredSize(new Dimension(1080, 600));
+
+        panel = new JPanel();
+        frame.add(panel);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 		frame.setVisible(true);
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(new Color(255, 0, 0));
+        g.drawRect(50, 50, 100, 50);
+     }
 
     public static void main(String[] args) {
         new Main();
