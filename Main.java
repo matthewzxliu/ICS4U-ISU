4b6911d565743af4644472fabce69c1aff8ce9e3
340854561 <340854561@gapps.yrdsb.ca>
2024-01-08 08:45:57 -0500

    Create window

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.*;
+
 public class Main
 {
+    JFrame frame;
+
+    public Main() {
+        frame = new JFrame("BomberMan");
+        frame.setPreferredSize(new Dimension(1080, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+		frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Fahad");
-        System.out.println("Fahard");
-        // Hello
+        new Main();
     }
 }
