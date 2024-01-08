960847bdcf692f7f236fe777fd6955411033ae7a
jfishB <jeeveshbalendra@gmail.com>
2024-01-08 09:36:18 -0500

    stuff need to fix

--- Main.java
+++ Main.java
@@ -1,32 +1,32 @@
 import java.util.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Main extends JPanel implements MouseListener
+public class Main extends JPanel implements MouseListener, KeyListener
 {
-    JFrame frame;
-    JPanel panel;
 
     static int xPos;
     static int yPos;
     static int gameState = 0;
+    public static BufferedImage player;
 
-    public Main() {
-        frame = new JFrame("BomberMan");
-        frame.setPreferredSize(new Dimension(1080, 600));
 
-        panel = new JPanel();
-        frame.add(panel);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-		frame.setVisible(true);
+    public Main() throws IOException{
+        setPreferredSize(new Dimension(450,720));
+        setFocusable(true);
+        addKeyListener(this);
+        addMouseListener(this);
+        Thread t = new Thread((Runnable) this);
+        t.start();
     }
 
     public void paintComponent(Graphics g) {
@@ -34,6 +34,7 @@ public class Main extends JPanel implements MouseListener
 
         g.setColor(new Color(255, 0, 0));
         g.drawRect(50, 50, 100, 50);
+        g.drawImage(player, 10, 10, null);
      }
 
     public void mousePressed(MouseEvent e) {
@@ -63,8 +64,29 @@ public class Main extends JPanel implements MouseListener
 	}
 	public void mouseExited(MouseEvent e) {		
 	}
+    public void keyTyped(KeyEvent e) {
+    }
+    public void keyPressed(KeyEvent e) {
+    }
+    public void keyReleased(KeyEvent e) {
+    }
 
-    public static void main(String[] args) {
-        new Main();
+    public static void main(String[] args) throws IOException{
+        JFrame frame = new JFrame("BomberMan");
+        frame.setPreferredSize(new Dimension(1080, 600));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+		frame.setVisible(true);
+        player = ImageIO.read(new File("boy_down_1.png"));
+    }
+
+    public void run() {
+        while(true) {
+            repaint();
+            try {
+                Thread.sleep(17);
+            } catch(Exception e) {}
+        }
     }
 }
