859889b5c929c4bc38489aa497183ff59f292765
340854561 <340854561@gapps.yrdsb.ca>
2024-01-09 09:18:53 -0500

    a

--- Main.java
+++ Main.java
@@ -14,70 +14,97 @@ import javax.swing.*;
 public class Main extends JPanel implements MouseListener, KeyListener
 {
 
-    static int xPos;
-    static int yPos;
-    static int gameState = 0;
+    static JFrame frame;
+	static int xPos;
+	static int yPos;
+	static int gameState = 0;
     public static BufferedImage player;
 
+	public Main() throws IOException
+	{
+		setPreferredSize(new Dimension(1080, 600));
+		setBackground(new Color(252, 177, 3));
 
-    public Main() throws IOException{
-        setPreferredSize(new Dimension(450,720));
         setFocusable(true);
         addKeyListener(this);
-        addMouseListener(this);
+		addMouseListener(this);
+
         Thread t = new Thread((Runnable) this);
         t.start();
-    }
+	}
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
 
-        g.setColor(new Color(255, 0, 0));
-        g.drawRect(50, 50, 100, 50);
-        g.drawImage(player, 10, 10, null);
-     }
+		// Play
+		g.setColor(new Color(160, 50, 168));
+		g.fillRect(410, 310, 260, 80);
+		
+		// About
+		g.setColor(new Color(160, 50, 168));
+		g.fillRect(410, 400, 260, 80);
+		
+		// Exit
+		g.setColor(new Color(160, 50, 168));
+		g.fillRect(410, 490, 160, 80);
+		
+		// Settings
+		g.setColor(new Color(160, 50, 168));
+		g.fillRect(580, 490, 90, 80);
+		
+		if(gameState == 1)
+		{
+			super.paintComponent(g);
+		}
+	}
 
     public void mousePressed(MouseEvent e) {
-        xPos = e.getX();
+		xPos = e.getX();
 		yPos = e.getY();
-
-        if(gameState == 0)
+		
+		if(gameState == 0)
 		{
-			if(xPos >= 150 && xPos <= 350 && yPos >= 375 && yPos <= 425)
+			if(xPos >= 410 && xPos <= 670 && yPos >= 310 && yPos <= 390)
+			{
+				gameState = 1;
+				repaint();
+			}
+			
+			else if(xPos >= 410 && xPos <= 670 && yPos >= 400 && yPos <= 480)
+			{
+				gameState = 1;
+				repaint();
+			}
+			
+			else if(xPos >= 410 && xPos <= 570 && yPos >= 490 && yPos <= 570)
+			{
+				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+			}
+			
+			else if(xPos >= 580 && xPos <= 670 && yPos >= 490 && yPos <= 570)
 			{
 				gameState = 1;
 				repaint();
 			}
 		}
+
 		else if(gameState == 1)
 		{
 			repaint();
 		}
 		repaint(); // This calls the paintComponent method
 	}
-	
-	public void mouseClicked(MouseEvent e) {		
-	}
-	public void mouseReleased(MouseEvent e) {		
-    }
-	public void mouseEntered(MouseEvent e) {		
-	}
-	public void mouseExited(MouseEvent e) {		
-	}
-    public void keyTyped(KeyEvent e) {
-    }
-    public void keyPressed(KeyEvent e) {
-    }
-    public void keyReleased(KeyEvent e) {
-    }
 
     public static void main(String[] args) throws IOException{
-        JFrame frame = new JFrame("BomberMan");
-        frame.setPreferredSize(new Dimension(1080, 600));
+        frame = new JFrame("Bomberman");
+        Main myPanel = new Main();
+        frame.add(myPanel);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 		frame.setVisible(true);
+        
         player = ImageIO.read(new File("boy_down_1.png"));
     }
 
@@ -88,5 +115,36 @@ public class Main extends JPanel implements MouseListener, KeyListener
                 Thread.sleep(17);
             } catch(Exception e) {}
         }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void mouseClicked(MouseEvent e) {		
+    }
+    public void mouseReleased(MouseEvent e) {		
+    }
+    public void mouseEntered(MouseEvent e) {		
+    }
+    public void mouseExited(MouseEvent e) {		
+    }
+    public void keyTyped(KeyEvent e) {
+    }
+    public void keyPressed(KeyEvent e) {
+    }
+    public void keyReleased(KeyEvent e) {
     }
 }
