0e6b2aadb0514053e4396c9291167b99815444c7
340854561 <340854561@gapps.yrdsb.ca>
2024-01-10 12:08:47 -0500

    a

--- Main.java
+++ Main.java
@@ -292,14 +292,9 @@ public class Main extends JPanel implements MouseListener, KeyListener, Runnable
 
 
 
-    public void mouseClicked(MouseEvent e) {		
-    }
-    public void mouseReleased(MouseEvent e) {		
-    }
-    public void mouseEntered(MouseEvent e) {		
-    }
-    public void mouseExited(MouseEvent e) {		
-    }
-    public void keyTyped(KeyEvent e) {
-    }
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void keyTyped(KeyEvent e) {}
 }
