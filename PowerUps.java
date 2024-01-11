9a3f6e16957d5fa28db82466407791fe827917f5
340854561 <340854561@gapps.yrdsb.ca>
2024-01-11 09:34:48 -0500

    a

---
+++ PowerUps.java
@@ -0,0 +1,20 @@
+public class PowerUps {
+    
+    private int playerVel;
+    private int enemyVel;
+
+    public int getPlayerVel()
+    {
+        return playerVel;
+    }
+
+    public int getEnemyVel()
+    {
+        return enemyVel;
+    }
+
+    public PowerUps(int playerVel, int enemyVel)
+    {
+
+    }
+}
